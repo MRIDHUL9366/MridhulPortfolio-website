7912f22d26df6714ddce6ccf2f03c85210e423d8
MRIDHUL9366 <mridhulmaddy8943@gmail.com>
2024-10-27 17:42:16 +0530

    Portfolio-website

---
+++ resume.js
@@ -0,0 +1,11 @@
+var SE=document.getElementByClassName("SE");
+var skills=document.getElementByClassName("skill-table");
+
+function opentap(tabname){
+      for(S of SE){
+      S.classList.remove("ME");
+       }
+       for(skill of Skills){
+      Skill.classList.remove("active-skill");
+       }
+}
